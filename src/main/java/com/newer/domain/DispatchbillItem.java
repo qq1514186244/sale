2fea7b76ef8dd04d5e68e14e7714c9a67cab8c9d
Your Name <email@example.com>
2019-04-08 17:09:26 +0800

    1

---
+++ src/main/java/com/newer/domain/DispatchbillItem.java
@@ -0,0 +1,42 @@
+package com.newer.domain;
+
+import java.io.Serializable;
+
+public class DispatchbillItem implements Serializable {
+    private int dbiid;
+    private Dispatchbill dispatchbill;
+    private DrugItem drugItem;
+    private String dbiquantity;
+
+    public int getDbiid() {
+        return dbiid;
+    }
+
+    public void setDbiid(int dbiid) {
+        this.dbiid = dbiid;
+    }
+
+    public Dispatchbill getDispatchbill() {
+        return dispatchbill;
+    }
+
+    public void setDispatchbill(Dispatchbill dispatchbill) {
+        this.dispatchbill = dispatchbill;
+    }
+
+    public DrugItem getDrugItem() {
+        return drugItem;
+    }
+
+    public void setDrugItem(DrugItem drugItem) {
+        this.drugItem = drugItem;
+    }
+
+    public String getDbiquantity() {
+        return dbiquantity;
+    }
+
+    public void setDbiquantity(String dbiquantity) {
+        this.dbiquantity = dbiquantity;
+    }
+}
